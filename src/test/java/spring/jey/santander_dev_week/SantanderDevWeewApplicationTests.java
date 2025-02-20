1ec1d7a6c224ad41b49738dc1d6e2b7a63f2385b
Jeymerson <jeymerson.jss@gmai.com>
2025-02-19 21:03:11 -0300

    primeiro commit

---
+++ src/test/java/spring/jey/santander_dev_week/SantanderDevWeewApplicationTests.java
@@ -0,0 +1,13 @@
+package spring.jey.santander_dev_week;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SantanderDevWeewApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
