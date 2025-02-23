e91e7102c2b0b2ab9bb37b023a945d1cbf887cde
Jeymerson <jeymerson.jss@gmai.com>
2025-02-23 17:17:26 -0300

    Bugfix CORS

--- src/main/java/spring/jey/santander_dev_week/Application.java
+++ src/main/java/spring/jey/santander_dev_week/Application.java
@@ -1,9 +1,15 @@
 package spring.jey.santander_dev_week;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
+import io.swagger.v3.oas.annotations.servers.Servers;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+//Para correção de CORS
+@OpenAPIDefinition(
+		servers = @Server(url = "/", description = "Default Server URL")
+)@SpringBootApplication
 public class Application {
 
 	public static void main(String[] args) {
